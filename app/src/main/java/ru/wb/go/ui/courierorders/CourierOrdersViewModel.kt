1b7d07e5c380646eb1bb0fca29622d261df33bac
Boris <ismailov.b4@wb.ru>
2022-06-08 12:15:56 +0300

    рабочая версия

--- app/src/main/java/ru/wb/go/ui/courierorders/CourierOrdersViewModel.kt
+++ app/src/main/java/ru/wb/go/ui/courierorders/CourierOrdersViewModel.kt
@@ -197,7 +197,7 @@ class CourierOrdersViewModel(
                     DialogInfoStyle.INFO.ordinal,
                     resourceProvider.getConfirmTitleDialog(courierOrderLocalEntity.id),
                     resourceProvider.getConfirmMessageDialog(
-                        CarNumberUtils(interactor.carNumber()).fullNumber(),
+                        CarNumberUtils(interactor.carNumber()).fullNumberWithMask(),
                         resourceProvider.getCargo(
                             courierOrderLocalEntity.minVolume,
                             courierOrderLocalEntity.minBoxesCount
