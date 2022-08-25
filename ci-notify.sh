8e4686f5309b2d208e40e12df05d23781e8d3dd2
Boris <ismailov.b4@wb.ru>
2022-08-25 14:08:05 +0300

    new realise

--- ci-notify.sh
+++ ci-notify.sh
@@ -1,11 +1,5 @@
 TIME="10"
-URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN"
+URL="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendMessage"
+TEXT="#apprelease Deploy status: $1%0A%0AProject:+$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
 
-TEXT="%0A%0AProject: WBGo ver.$2%0A [Download from Gitlab]($CI_PROJECT_URL/-/jobs/$1/artifacts/download)%0ABranch:+$CI_COMMIT_REF_SLUG%0A%0A#apprelease"
-
-msg_id=$(curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&parse_mode=markdown&text=$TEXT" $URL/sendMessage | jq '.result.message_id')
-
-curl -v -F "chat_id=$TELEGRAM_USER_ID" -F document=@$3 $URL/sendDocument
-curl -v -F "chat_id=$TELEGRAM_USER_ID" -F document=@$4 $URL/sendDocument
-curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&message_id=$msg_id" $URL/pinChatMessage
-
+curl -s --max-time $TIME -d "chat_id=$TELEGRAM_USER_ID&disable_web_page_preview=1&text=$TEXT" $URL > /dev/null
